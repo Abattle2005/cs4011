8f5f3ed4648373b4e2be589fa74d05c3e2e5fe1e
sciart <sciart@gmail.com>
2026-01-05 00:02:11 -0600

    [chapter-08] rsjx firstValueFrom

--- app_ng/src/app/loc8r-data.service.ts
+++ app_ng/src/app/loc8r-data.service.ts
@@ -17,11 +17,6 @@ export class Loc8rDataService {
     const lat: number = 51.378091;
     const maxDistance: number = 20000;
     const url: string = `${this.apiBaseUrl}/locations?lng=${lng}&lat=${lat}&maxDistance=${maxDistance}`;
-    // return this.http
-    //   .get(url)
-    //   .toPromise()
-    //   .then(response => response as Location[])
-    //   .catch(this.handleError);
     return firstValueFrom(this.http.get<Location[]>(url))
       .catch(this.handleError);
   }
@@ -30,11 +25,6 @@ export class Loc8rDataService {
     const url: string = `${this.apiBaseUrl}/locations/${locationId}`;
     return firstValueFrom(this.http.get<Location[]>(url))
       .catch(this.handleError);
-    // return this.http
-    //   .get(url)
-    //   .toPromise()
-    //   .then(response => response as Location)
-    //   .catch(this.handleError);
   }
 
   public addReviewByLocationId(locationId: string, formData: Review): Promise<Review> {
