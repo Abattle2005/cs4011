b4ac75a25faecec305e05f7f12db8ed3608a2e8d
sciart <sciart@gmail.com>
2026-03-11 23:44:38 -0500

    [chapter-10] clean up

--- app_ng/src/app/page-header/page-header.component.ts
+++ app_ng/src/app/page-header/page-header.component.ts
@@ -15,22 +15,3 @@ export class PageHeaderComponent implements OnInit {
   ngOnInit() {
   }
 }
-
-
-// import { Component, OnInit, Input } from '@angular/core';
-
-// @Component({
-//   selector: 'app-page-header',
-//   templateUrl: './page-header.component.html',
-//   styleUrls: ['./page-header.component.css']
-// })
-// export class PageHeaderComponent implements OnInit {
-
-//   @Input() content: any;
-
-//   constructor() { }
-
-//   ngOnInit() {
-//   }
-
-// }
